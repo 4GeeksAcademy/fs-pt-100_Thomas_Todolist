88b73f25bc7d9afb9cada960003fd8da090a0cc2
timo131 <thomasemosley3@gmail.com>
2025-04-26 20:19:49 +0000

    javi details applied

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,34 +1,39 @@
 import React, { useState } from "react";
 
-//create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState("")
-	const [ todos, setTodos ] = useState([])
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		if (inputValue !== "") {
+			setTodos([...todos, inputValue]);
+			setInputValue("");
+		}
+	}
+
 	const deleteTodo = (todoIndex) => {
 		const newTodos = todos.filter((_, index) => index !== todoIndex);
 		setTodos(newTodos);
 	}
+
 	return (
 		<div className="container col-6">
 			<h1 className="my-3">My Dynamic To-Do List</h1>
-			<ul>
-				<li>
-					<input
-						type="text"
-						name="input"
-						placeholder="What do you need to do?"
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyDown={(e) => {
-							if (e.key == "Enter" && e.target.value !== "") {
-								setTodos(todos.concat(inputValue));
-								setInputValue("");
-							}
-						}}
-					></input>
-				 </li>
-				 {todos.map((todoItem, index) => (
-					<li key={index}>
+			<form onSubmit={handleSubmit}>
+				<input
+					className="form-control"
+					type="text"
+					name="input"
+					placeholder="What do you need to do?"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={inputValue}
+				/>
+			</form>
+			<h6 className="mt-4 text-center">Pending tasks:</h6>
+			<ul className="list-group">
+				{todos.map((todoItem, index) => (
+					<li key={index} className="list-group-item">
 						<div className="d-flex justify-content-between align-items-center">
 							<p>{todoItem}</p>
 							<span className="fas fa-trash" onClick={() => deleteTodo(index)}></span>
@@ -36,7 +41,7 @@ const Home = () => {
 					</li>
 				))}
 			</ul>
-			<div>Tasks to do: {todos.length}</div>
+			<p className="text-center">Tasks to do: {todos.length}</p>
 		</div>
 	);
 
